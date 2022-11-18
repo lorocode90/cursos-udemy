b10047cac606e437a638d3497df5a8e465e75a96
omar.lopez <loro.code@gmail.com>
2022-11-17 23:14:32 -0600

    update nest js

---
+++ reactjs/03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const FirstApp = () => {
+	return <div>FirstApp</div>;
+};
---
+++ reactjs/03-counter-app-vite/src/HelloWorldApp.jsx
@@ -0,0 +1,16 @@
+import { Fragment } from 'react';
+
+function HelloWorldApp() {
+	return (
+		<Fragment>
+			<h1>Hola Mundo</h1>
+			<h2>Fragment</h2>
+		</Fragment>
+	);
+}
+export default HelloWorldApp;
+// import React from 'react';
+
+// export const HelloWorldApp = () => {
+// 	return <div>HelloWorldApp</div>;
+// };
